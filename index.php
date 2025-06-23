0570f535fbf41417e64216d02cb7b74d97cd092a
yogadwi24 <54340020+yogadwi24@users.noreply.github.com>
2025-06-24 02:18:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,197 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="assets/img/logo.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Viga&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins|Viga&display=swap" rel="stylesheet">
+    <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/notojavanese" type="text/css" />
+
+    <!-- My css -->
+    <link rel="stylesheet" href="style1.css">
+
+    <title> GPE </title>
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container">
+            <img src="assets/img/GPE Blank BG.png" width="250" height="60"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-item nav-link active" href="#home">Home <span class="sr-only">(current)
+                        </span></a>
+                    <a class="nav-item nav-link" href="#industri">Industri </a>
+                    <a class="nav-item nav-link" href="#explore">Explore </a>
+                    <a class="nav-item nav-link" href="#about">About </a>
+                    <a class="nav-item btn btn-primary tombol" href="login2.php">Login </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <!-- navbar -->
+
+    <!-- jumbotron -->
+    <div class="jumbotron jumbotron-fluid height100p banner" id="home">
+        <div class="container h100">
+            <div class="contentBox h100">
+                <div class="headerText text-center">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- container -->
+    <div class="container" id="home">
+        <div class="row justify-content-center">
+            <div class="col-lg-10 info-gpe">
+                <div class="gpe text-center">
+                    <h2>GPE (Go Purbalingga Explore)</h2>
+                    <p> Mempermudah pengguna untuk menemukan beberapa fasilitas umum dan destinasi tempat-tempat yang recomended di Purbalingga. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- akhir container -->
+
+    <!-- industri -->
+    <section class="sec" id="industri">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center industri">
+                    <img src="img/sapu.jpeg" class="img-fluid">
+                </div>
+                <div class="col-md-6 text-justify">
+                    <h3>Industri Sapu Purbalingga</h3>
+                    <p>Pengrajin sapu di Desa Kajongan, Kecamatan Bojongsari, Purbalingga, Jawa Tengah, memilah dan mengikat batang tanaman gandum untuk dijadikan kerajinan sapu. Sapu Hamada yang muncul sejak tahun 1969 bersama dengan jenis kerajinan sapu
+                        lainnya seperti sapu ijuk yang terbuat dari ijuk pohon aren, serta sapu glalah terbuat dari tanaman rumput, satu dari beberapa jenis sapu lainnya yang dibuat pengrajin sudah menembus pasar diekspor hingga ke Jepang.</p>
+                </div>
+                <div class="col-md-6 text-justify">
+                    <h3>Purbalingga, produsen knalpot yang legendaris di Indonesia</h3>
+                    <p>Tidak hanya menguasai pasar domestik namun hingga mancanegara. Keunggulan knalpot Purbalingga, selain mampu meningkatkan tenaga mobil juga suara knalpot yang bisa diatur.Industri knalpot di Purbalingga masuk industri rumahan. Sentra
+                        industri terletak di Dusun Pesayangan Kelurahan Purbalingga Lor, Purbalingga. Industri knalpot merupakan transformasi dari industri kuali dan panci tembaga yang ada sejak tahun 1950-an. Karena itu, knalpot merupakan usaha turun-temurun
+                        digeluti warga</p>
+                </div>
+                <div class="col-md-6 ">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ZmAE74lyNQo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <!-- industri akhir -->
+    <section class="sec1" id="explore">
+        <div class="container">
+            <div class="row">
+                <div class="offset-sm-2 col-sm-8">
+                    <div class="headerText text-center">
+                        <h2>Wow Purbalingga</h2>
+                        <p>
+                            Purbalingga yang masuk dalam kawasan wisata Jawa Tengah ini menyimpan banyak destinasi tempat wisata yang menarik untuk dikunjungi. Mulai dari wisata buatan, wisata alam, wisata religi, hingga desa wisata.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="box">
+                <div class="imgBx">
+                    <img src="img/Owabong-Purbalingga.jpg">
+                </div>
+                <div class="content">
+                    <h3>Owabong Waterpark</h3>
+                    <p>Owabong, Salah satu tempat wisata yang menjadi icon dari kota Purbalingga yang terletak di Desa Bojongsari, Kecamatan Bojongsari. Yang merupakan sebuah taman wisata yang umumnya sebagian besar didominasi oleh wisata air. Terdapat beberapa
+                        wahana air yang populer seperti Kolam renang olympic, waterboom, ember tumpah, kolam arus dan masih banyak lagi.</p>
+                </div>
+            </div>
+            <div class="box">
+                <div class="imgBx">
+                    <img src="img/Owabong-Purbalingga.jpg">
+                </div>
+                <div class="content">
+                    <h3>Owabong Waterpark</h3>
+                    <p>Owabong, Salah satu tempat wisata yang menjadi icon dari kota Purbalingga yang terletak di Desa Bojongsari, Kecamatan Bojongsari. Yang merupakan sebuah taman wisata yang umumnya sebagian besar didominasi oleh wisata air. Terdapat beberapa
+                        wahana air yang populer seperti Kolam renang olympic, waterboom, ember tumpah, kolam arus dan masih banyak lagi.</p>
+                </div>
+            </div>
+            <div class="box">
+                <div class="imgBx">
+                    <img src="img/Owabong-Purbalingga.jpg">
+                </div>
+                <div class="content">
+                    <h3>Owabong Waterpark</h3>
+                    <p>Owabong, Salah satu tempat wisata yang menjadi icon dari kota Purbalingga yang terletak di Desa Bojongsari, Kecamatan Bojongsari. Yang merupakan sebuah taman wisata yang umumnya sebagian besar didominasi oleh wisata air. Terdapat beberapa
+                        wahana air yang populer seperti Kolam renang olympic, waterboom, ember tumpah, kolam arus dan masih banyak lagi.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="sec2" id="about">
+        <div class="container h100">
+            <div class="contentBox h100">
+                <div>
+                    <h2>GPE (Go Purbalingga Explore)</h2>
+                    <p>GPE (Go Purbalingga Explore) dapat memudahkan pengguna untuk menemuan beberapa fasilitas umum. Pengguna dapat login terlebih dahulu untuk melihat lokasi tempat ia berada, dapat menentukan rute terdekat menuju tempat yang ingin dituju
+                        dan juga dapat menyarankan lokasi yang menjadi rekomendasi untuk dikunjungi dengan mengguanakn fitur Google Direction di sekitar wilayah tempat pengguna berada. GPE juga dilengkapi dengan informasi keterangan yang menandakan lokasi
+                        tersebut valid dan benar adanya.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <ul class="sci">
+                        <li><a href="https://www.facebook.com/GPE_map"><i class="fa fa-facebook"></i></a></li>
+                        <!-- use fontawesome icon -->
+                        <li><a href="https://www.twitter.com/GPE_map"><i class="fa fa-twitter"></i></a></li>
+                        <!-- use fontawesome icon -->
+                        <li><a href="https://www.googleplus.com/GPE_map"><i class="fa fa-google-plus"></i></a></li>
+                        <!-- use fontawesome icon -->
+                        <li><a href="https://www.instagram.com/GPE_map"><i class="fa fa-instagram"></i></a></li>
+                        <!-- use fontawesome icon -->
+                    </ul>
+                    <p class="cpryt">
+                        Copyright © 2019 GPE.com | All Rights Reserved by <a href="#">IF16 ITTelkomPWT</a>
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- jumotron akhir-->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script type="text/javascript">
+        $(document).scroll(function() {
+            $('.navbar').toggleClass('scrolled', $(this).scrollTop() > $('.navbar').height())
+        })
+    </script>
+</body>
+
+</html>
